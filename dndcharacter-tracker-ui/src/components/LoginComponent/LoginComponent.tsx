881413a73940d6be95ddc7bfa6c660d2d8e2478e
ABlanton3 <a.m.blanton@gmail.com>
2020-07-13 21:26:46 -0500

    started login component and index, installed a few more things needed for the app

---
+++ dndcharacter-tracker-ui/src/components/LoginComponent/LoginComponent.tsx
@@ -0,0 +1,13 @@
+import React, { FunctionComponent, useState } from 'react'
+
+export const LoginComponent:FunctionComponent<any> = (props) => {
+    const [username, changeUsername] = useState('')
+    const [password, changePassword] = useState('')
+
+    return(
+        <div>
+
+
+        </div> //go find a good, pretty text field to put in there & make sure you password up the password
+    )
+}
